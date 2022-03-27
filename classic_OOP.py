2a4b84b553e4d30f81caf4d706ce94eecd368a19
mauroebordon <mauro.e.bordon@gmail.com>
2022-03-27 20:50:05 -0300

    working project

--- classic_OOP.py
+++ classic_OOP.py
@@ -1,11 +1,11 @@
+import time
 import ccxt.async_support as ccxt 
-from asyncio import gather, get_event_loop
+from asyncio import gather, get_event_loop, AbstractEventLoop
 from enum import Enum
-#from typing import List
+from typing import List
 
 
 class Coins(str, Enum):
-#TRANSFORMAR ESTO EN OOP BASICAMENTE
     BTC = 'BTC'
     ETH = 'ETH'
     ADA = 'ADA'
@@ -17,78 +17,63 @@ class Coins(str, Enum):
     BNB = 'BNB'
 
 class Ticker:
-    def __init__(self, c1: Coins, c2: Coins):
-        self.fst_coin = c1
-        self.snd_coin = c2
-        self.symbol = str(c1)+"/"+str(c2)
+    def __init__(self, c1: Coins, c2: Coins, exchange_id='binance'):
+        self.c1 = f'{c1}/USDT'
+        self.c2 = f'{c2}/USDT'
+        self.symbol = f'{c1}/{c2}'
+        self.exchange = None
+        self.exchange_id = exchange_id
+        self.ticker = {}
+        self.now = None
+        self.bids = {}
+        self.asks = {}
         
+    async def run_loops(self, asyncio_loop: AbstractEventLoop):
+        loops = [self.exchange_loop( asyncio_loop,
+                               [self.c1, self.c2, self.symbol])]
+        await gather(*loops)
+        
+    async def exchange_loop(self, asyncio_loop: AbstractEventLoop,  symbols: List[str]):
+        self.exchange = getattr(ccxt, self.exchange_id)({
+            'enableRateLimit': True,
+            'asyncio_loop': asyncio_loop,
+        })
+        loops = [self.orderbook_loop(symbol) for symbol in symbols]
+        await gather(*loops)
+        await self.exchange.close()  
+        
+    async def orderbook_loop(self, symbol: str):
+        while True:
+            try:
+                self.ticker = await self.exchange.fetch_ticker(symbol)
+                
+                self.now = self.ticker["datetime"]
+                self.asks[symbol] = self.ticker['bid']
+                self.bids[symbol] = self.ticker['ask']
+                print(self)
+
+            except Exception as e:
+                print(str(e))
+                # # raise e  # uncomment to break all loops in case of an error in any one of them
+                break  # you can break just this one loop if it fails
+
+
+    
     def __str__(self):
-        return str.upper(self.name)
-    
-    def get_bid_price(self):
-        """
-        get the bid price in USDT
-        """
-        
-        pass
-    
-    def get_ask_price(self):
-        """
-        get the ask price in USDT
-        """
-        pass
-
-class Ratios:
-    pass
-
-
-async def symbol_loop(exchange, symbol):
-    print('Starting the', exchange.id, 'symbol loop with', symbol)
-    while True:
-        try:
-            orderbook = await exchange.fetch_order_book(symbol)
-            now = exchange.milliseconds()
-            print(exchange.iso8601(now), exchange.id, symbol, orderbook['asks'][0], orderbook['bids'][0])
-
-            # --------------------> DO YOUR LOGIC HERE <------------------
-
-        except Exception as e:
-            print(str(e))
-            # raise e  # uncomment to break all loops in case of an error in any one of them
-            break  # you can break just this one loop if it fails
-
-async def exchange_loop(asyncio_loop, exchange_id, symbols):
-    print('Starting the', exchange_id, 'exchange loop with', symbols)
-    exchange = getattr(ccxt, exchange_id)({
-        'enableRateLimit': True,
-        'asyncio_loop': asyncio_loop,
-    })
-    loops = [symbol_loop(exchange, symbol) for symbol in symbols]
-    await gather(*loops)
-    await exchange.close()
-
-
-async def main(asyncio_loop):
-    exchanges = {
-        'binance': ['BTC/USDT', 'ETH/USDT', 'ETH/BTC'],
-    }
-    loops = [exchange_loop(asyncio_loop, exchange_id, symbols) for exchange_id, symbols in exchanges.items()]
-    await gather(*loops)
+        return f'''-----
+Date/Time: {self.now} 
+Exchange: {str.upper(self.exchange.id)}
+Ticker Cryptos: {self.symbol}
+Ratio1: {self.bids[self.c1]/self.asks[self.c2]:.6f}
+Ratio2: {self.bids[self.c2]/self.asks[self.c1]:.6f}
+-----                
+                '''
 
 
 if __name__ == '__main__':
+    #buscar que sea c1/c2 o c2/c1 es importante
+    ticker = Ticker("ETH", "BTC")
     asyncio_loop = get_event_loop()
-asyncio_loop.run_until_complete(main(asyncio_loop))
+    asyncio_loop.run_until_complete(ticker.run_loops(asyncio_loop))
     
-# if __name__ == '__main__':
-#     binance = ccxt.binance({
-#         'enableRateLimit': True,  # this option enables the built-in rate limiter
-#     })
-#     usdt = 'USDT'
-#     coin1 = 'BTC'
-#     coin2 = 'ETC'
-#     tickers = [Ticker(coin1,coin2), Ticker(coin2,coin1), Ticker(coin1,usdt), Ticker(coin2,usdt)]
-#     for ticker in tickers:
-#         asyncio.get_event_loop().run_until_complete(run(binance, ticker))
-#     pass
-
+    
